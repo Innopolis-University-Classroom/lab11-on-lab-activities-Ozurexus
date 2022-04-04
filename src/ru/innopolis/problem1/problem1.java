82e66d6328667e9fe9a703f2a238dd6c8a5d0066
MaximPinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-04-04 13:32:20 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,27 @@
-// your solution here
-//fix all the errors including package errors
+import java.util.Locale;
+import java.util.Scanner;
+interface Operation{
+    default void operate(String s){
+
+    }
+}
+class Workshop implements Operation {
+    private Operation workshop;
+    public void setWorkshop(String message){
+        System.out.println(message);
+    }
+    //workshop;
+}
+public class Main {
+    public static void main(String[] args) {
+        Workshop carWorkshop = new Workshop();
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        if (next.toUpperCase().equals("A")){
+            carWorkshop.setWorkshop("we Should assembles it");
+        }
+        else if (next.toUpperCase().equals("D")){
+            carWorkshop.setWorkshop("we Should disassembles it");
+        }
+    }
+}
