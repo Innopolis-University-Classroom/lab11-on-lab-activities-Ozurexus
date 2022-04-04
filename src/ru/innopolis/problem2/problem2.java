a1016abe24f77fb1f9222c7493dd6ff104bbecd6
MaximPinyaguin <88331457+Ozurexus@users.noreply.github.com>
2022-04-04 13:52:02 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,17 @@
-// your solution here
-//fix all the errors including package errors
+class ChargerStation{
+       void chargeByLightning(LightningPort P){
+
+       }
+    }
+    class iPhone implements LightningPort{
+        public void chargeByLightning(){}
+    }
+    class Android implements UsbPort{
+        public void chargebyUsb(){}
+    }
+    interface LightningPort{
+         void chargeByLightning();
+    }
+    interface UsbPort{
+         void chargebyUsb();
+    }
